0115976c4bc04d665b9a39e4e0dd47c11ec755a7
Pavel P <legeon48@mail.ru>
2023-01-11 14:31:38 +0300

    Добавьте файлы проекта.

---
+++ CmsShoppingCart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CmsShoppingCart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
